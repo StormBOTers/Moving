a4ce321c9e1aa621fa3ab6d06daf2aa2257c159e
silvapedro736 <736silvapedro@gmail.com>
2015-10-31 13:48:18 -0400

    “

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Move.java
@@ -0,0 +1,39 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by pedrosilva on 10/31/15.
+ */
+public class Move extends OpMode {
+
+    DcMotor motorRight;
+    DcMotor motorLeft;
+
+
+    public void init() {
+        motorRight = hardwareMap.dcMotor.get("motor_right");
+        motorLeft = hardwareMap.dcMotor.get("motor_left");
+
+
+    }
+
+    public void loop() {
+
+        float P = 1;
+
+        motorRight.setPower(1);
+        motorLeft.setPower(1);
+
+
+    }
+
+    public void stop() {
+
+    }
+
+
+}
